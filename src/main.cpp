a17be7b16047b927be5177a71786ac5561c4288a
GABRIEL <gabriel-moreira98@hotmail.com>
2020-03-04 14:17:48 -0400

    Commit feito

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,9 @@ int angulo=15; // Ajusta o ângulo inicial do Servo na parte superior
 int tetaeixo=60;//Ajusta o angulo inicial do servo na base 
 float passo=3;
 
+int distanceX = 0;
+int distanceY = 0;
+
 Fuzzy *fuzzy = new Fuzzy();
 
 void setup() {
@@ -40,15 +43,15 @@ void setup() {
   // Instantiating a FuzzyOutput objects
   FuzzyOutput *speed = new FuzzyOutput(1);
   // Instantiating a FuzzySet object
-  FuzzySet *slow = new FuzzySet(1, 1, 5, 8);
+  FuzzySet *slow = new FuzzySet(1, 1, 3, 5);
   // Including the FuzzySet into FuzzyOutput
   speed->addFuzzySet(slow);
   // Instantiating a FuzzySet object
-  FuzzySet *average = new FuzzySet(5, 8, 8, 10);
+  FuzzySet *average = new FuzzySet(3, 5, 5, 8);
   // Including the FuzzySet into FuzzyOutput
   speed->addFuzzySet(average);
   // Instantiating a FuzzySet object
-  FuzzySet *fast = new FuzzySet(8, 12, 12, 15);
+  FuzzySet *fast = new FuzzySet(5, 8, 8, 10);
   // Including the FuzzySet into FuzzyOutput
   speed->addFuzzySet(fast);
   // Including the FuzzyOutput into Fuzzy
@@ -96,80 +99,83 @@ void setup() {
   // Including the FuzzyRule into Fuzzy
   fuzzy->addFuzzyRule(fuzzyRule03);
 }
-  
+
 void loop() { 
   int LEFT = analogRead(A0);
   int UP = analogRead(A1);
   int DOWN = analogRead(A2);
   int RIGHT = analogRead(A3);
-  /*Serial.print(LEFT);
-  Serial.print(' ');
-  Serial.print(UP);
-  Serial.print(' ');
-  Serial.print(DOWN);
-  Serial.print(' ');
-  Serial.print(RIGHT);
-  Serial.println(' ');
-  //Serial.println(LDR5);
-  */
-  // delay(50);
+  
   ////////////////EIXO INFERIRO DO PROJETO 
 
   if (abs(RIGHT-LEFT)<40){
-    delay(200);
+    Serial.println("Eixo X OK");
+    // cont1 = false;
+    // delay(200);
   }
   else if((RIGHT>LEFT) && (tetaeixo>30)){
-    Serial.print("Diferença: ");
-    Serial.println(RIGHT - LEFT);
+    // Serial.print("Diferença: ");
+    // Serial.println(RIGHT - LEFT);
+    distanceX = RIGHT - LEFT;
     fuzzy->setInput(1, RIGHT - LEFT);
     fuzzy->fuzzify();
     passo = fuzzy->defuzzify(1);
     tetaeixo -= passo;
-    Serial.print("DIREITA: ");
-    Serial.println(passo);
+    // Serial.print("DIREITA: ");
+    // Serial.println(passo);
     meuservo1.write(tetaeixo);
-    delay(25);
+    // delay(25);
   }
   else if((LEFT>RIGHT)  && (tetaeixo<100)){
-    Serial.print("Diferença: ");
-    Serial.println(LEFT - RIGHT);
+    // Serial.print("Diferença: ");
+    // Serial.println(LEFT - RIGHT);
+    distanceX = RIGHT - LEFT;
     fuzzy->setInput(1, LEFT - RIGHT);
     fuzzy->fuzzify();
     passo = fuzzy->defuzzify(1);
     tetaeixo += passo;
-    Serial.print("ESQUERDA: ");
-    Serial.println(passo);
+    // Serial.print("ESQUERDA: ");
+    // Serial.println(passo);
     meuservo1.write(tetaeixo);
-    delay(25);
+    // delay(25);
   }
 
   /////////////EIXO SUPERIOR RESPONSAVEL PELO ANGULO
 
   if (abs(UP-DOWN)<20){
-    delay(200);
+    // cont2 = false;
+    // delay(200);
+    Serial.println("Eixo Y OK");
   }
   else if((UP>DOWN) && (angulo<55)){
-    Serial.print("Diferença: ");
-    Serial.println(UP - DOWN);
+    // Serial.print("Diferença: ");
+    // Serial.println(UP - DOWN);
+    distanceY = UP - DOWN;
     fuzzy->setInput(1, UP - DOWN);
     fuzzy->fuzzify();
     passo = fuzzy->defuzzify(1);
     angulo += passo;
-    Serial.print("UP: ");
-    Serial.println(passo);
+    // Serial.print("UP: ");
+    // Serial.println(passo);
     meuservo2.write(angulo);
-    delay(25);
+    // delay(25);
   }
   else if((DOWN>UP) && (angulo>2)){
-    Serial.print("Diferença: ");
-    Serial.println(DOWN - UP);
+    // Serial.print("Diferença: ");
+    // Serial.println(DOWN - UP);
+    distanceY = UP - DOWN;
     fuzzy->setInput(1, DOWN - UP);
     fuzzy->fuzzify();
     passo = fuzzy->defuzzify(1);
     angulo -= passo;
-    Serial.print("DOWN: ");
-    Serial.println(passo);
+    // Serial.print("DOWN: ");
+    // Serial.println(passo);
     meuservo2.write(angulo);
-    delay(25);
+    // delay(25);
   }
+
+  Serial.print(distanceX);
+  Serial.print(", ");
+  Serial.println(distanceY);
+  delay(50);
 }  
